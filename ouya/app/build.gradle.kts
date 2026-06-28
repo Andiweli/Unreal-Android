8bd55d538d9ecb5d81a9f2b2bb79f8b79c2154ce
Andiweli <stuermer.andreas@gmail.com>
2026-06-28 12:30:09 +0200

    Update to 1.3.2

--- ouya/app/build.gradle.kts
+++ ouya/app/build.gradle.kts
@@ -669,7 +669,7 @@ android {
         minSdk = 16
         targetSdk = 19
         versionCode = 1
-        versionName = "1.3.1"
+        versionName = "1.3.2"
 
         ndk {
             abiFilters += listOf("armeabi-v7a")
